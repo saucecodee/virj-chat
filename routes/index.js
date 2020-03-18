85b776a09dc12cbdc6d3e6dd9f9c0ba95aa145eb
saucecodee <ashawinta@gmail.com>
2020-03-18 02:01:21 +0100

    set up socket event listners

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 
-module.exports = function() {
+module.exports = function (server) {
   router.get("/test", (req, res) => {
     res.send("yehh!");
   });
   router.use("/users", userRoutes());
+
+  const io = require('socket.io')(server);
+
+  io.on('connection', (socket) => {
+
+    socket.on('user-left', () => {
+      io.emit('movement', { user: socket.username, event: 'left' });
+    });
+
+    socket.on('user-created', (name) => {
+      io.emit('movement', { user: socket.username, event: 'joined' });
+    });
+
+    socket.on('send-message', (message) => {
+      io.emit('message', { msg: message.text, user: socket.username, sentAt: new Date() });
+    });
+  });
 
   return router;
 };
