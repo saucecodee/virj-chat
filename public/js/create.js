bb0e93aa6414725074425ee5f5b998b4651387e1
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-03-20 01:59:45 +0100

    undo the last commit

--- public/js/create.js
+++ public/js/create.js
@@ -1,10 +1,29 @@
 const $ = n => document.querySelector(n);
 
+function snack(text){
+  $('.snack-bar').style.display = "block";
+  $('.snack-bar').textContent = text;
+
+  const timed = setTimeout(()=> $('.snack-bar').style.display = "none", 2000);
+}
+
 $('.create-group-button').onclick = e => {
   e.preventDefault();
+  const val = $('.create-form')['username'].value.trim();
 
-  // Check for server responce
-  if (true){
-    
+  if (val.length > 0) {
+    // Check for server responce
+    if (true) {
+      // dummy group id
+      const groupID = "dihf4"
+      $('.success-cont').style.display = "block";
+      $('.group-id-cont').textContent = `Your group id is ${groupID}`;
+    }
+    else {
+      $('.failure-cont').style.display = "block";
+    }
+  }
+  else {
+    snack("Please write a username")
   }
 }
---
+++ public/js/join.js
@@ -0,0 +1,29 @@
+const $ = n => document.querySelector(n);
+
+function snack(text) {
+  $('.snack-bar').style.display = "block";
+  $('.snack-bar').textContent = text;
+
+  const timed = setTimeout(() => $('.snack-bar').style.display = "none", 2000);
+}
+
+
+$('.create-group-button').onclick = e => {
+  e.preventDefault();
+  const val1 = $('.create-form')['username'].value.trim();
+  const val2 = $('.create-form')['group-code'].value.trim();
+
+
+  if (val1.length > 0 && val2.length > 0) {
+    // Check for server responce
+    if (true) {
+      // navigate to that group
+    }
+    else {
+      $('.failure-cont').style.display = "block";
+    }
+  }
+  else {
+    snack("Please fill the form")
+  }
+}
