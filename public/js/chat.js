d9a0666e5df56357c93587ccf977b196d385b3ba
Anthony <johnanthonyxplorer@gmail.com>
2020-03-21 11:32:22 +0100

    still trying to fix my json bug

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,35 @@
+let messageInput = document.querySelector('#message')
+let btnSubmit = document.querySelector('#btn-submit')
+let messages = document.querySelector('#messages')
+
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(messageInput.value);
+  sendMessage(messageInput.value)
+  messageInput.value = ''
+})
+
+function sendMessage(text) {
+  let content = text;
+  let d = new Date();
+  let div = document.createElement('div')
+  div.classList.add('message', 'sent')
+  let message = `
+          <div class="arrow"></div>
+          <div class="sent-by"></h4>vince{}</div>
+          <p> ${content} </p>
+          <div class="sent-time">${d.getHours()}:${d.getMinutes()}${d.getHours() >= 12 ? 'pm' : 'am'} </div>
+
+  `;
+  div.innerHTML = message;
+  messages.appendChild(div);
+  div.scrollIntoView();
+}
+
+function scrollToBottom() {
+  messages.lastElementChild.scrollIntoView()
+}
+
 let membersList = document.getElementById("members");
 let membersbtn = document.getElementById("checkMembers");
 
@@ -8,3 +40,17 @@ function showMembers(){
 function hideMembers(){
     membersList.style.display = "none"
 }
+
+membersbtn.addEventListener('click', updateMembers);
+
+function updateMembers(){
+    fetch('villagePeople.json')
+    .then((res) => res.text())
+    .then((data) => {
+        console.log(data);
+        
+    })
+}
+
+
+
