602bcdb48d199eaacf127273f1b518a7f2d08c58
saucecodee <ashawinta@gmail.com>
2020-03-27 20:31:02 +0100

    Refactored chat page UI

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,14 @@
 // import { json } from "express";
 const $ = n => document.querySelector(n);
 let messageInput = $('.message-input')
-let submitBtn = $('.submit-btn')
+let messageForm = $('.message-form')
 let messages = $('.chat-body')
 let memberContainer = $('#listContainer');
 
-
-
 let groupDetails = JSON.parse(localStorage.getItem('data'))
 
-console.log(groupDetails);
 
-// let currentCode = $('#groupCode').innerHTML = groupDetails.code;
-// let currentCode = $('#groupName').innerHTML = `${groupDetails.groupName}  (${groupDetails.code})`;
-
-// console.log(currentCode);
-$('.menu').onclick = () => {
-
-  if ($('.group-actions-menu').style.display === "none") {
-    $('.group-actions-menu').style.display = "block"
-  }
-  else {
-    $('.group-actions-menu').style.display = "none"
-  }
-}
-
-submitBtn.addEventListener('click', (e) => {
+messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (messageInput.value.length !== 0) {
     sendMessage(messageInput.value)
